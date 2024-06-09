785340082f4b257bfb27927471614158d17cee55
orito <oritor91@gmail.com>
2024-06-09 22:43:48 +0300

    changing the version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='telegram_jellyfin_bot',
-    version='0.1.1',
+    version='0.1.0',
     packages=find_packages(),
     install_requires=[
         'python-telegram-bot>=13.0',
